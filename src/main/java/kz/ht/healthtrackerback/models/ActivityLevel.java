808ba13023d88f4cab9dae59b6d768fa05732cd2
Duman <baltabayduman@mail.ru>
2024-05-08 04:39:48 +0600

    add db

--- src/main/java/kz/ht/healthtrackerback/models/ActivityLevel.java
+++ src/main/java/kz/ht/healthtrackerback/models/ActivityLevel.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "activity_level")
 public class ActivityLevel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String description;
--- src/main/java/kz/ht/healthtrackerback/models/GeneralPurposeType.java
+++ src/main/java/kz/ht/healthtrackerback/models/GeneralPurposeType.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "general_purpose_type")
 public class GeneralPurposeType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
 }
